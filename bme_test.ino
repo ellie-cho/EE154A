c696152ed6b24e31f8d9f372df34e7f87c9556ea
Adam Krivka <akrivka@caltech.edu>
2024-01-31 19:14:43 -0800

    bme_test code

---
+++ bme_test.ino
@@ -0,0 +1,104 @@
+/*
+BME280 I2C Test.ino
+
+This code shows how to record data from the BME280 environmental sensor
+using I2C interface. This file is an example file, part of the Arduino
+BME280 library.
+
+GNU General Public License
+
+Written: Dec 30 2015.
+Last Updated: Oct 07 2017.
+
+Connecting the BME280 Sensor:
+Sensor              ->  Board
+-----------------------------
+Vin (Voltage In)    ->  3.3V
+Gnd (Ground)        ->  Gnd
+SDA (Serial Data)   ->  A4 on Uno/Pro-Mini, 20 on Mega2560/Due, 2 Leonardo/Pro-Micro
+SCK (Serial Clock)  ->  A5 on Uno/Pro-Mini, 21 on Mega2560/Due, 3 Leonardo/Pro-Micro
+
+ */
+
+#include <BME280I2C.h>
+#include <Wire.h>
+
+#define SERIAL_BAUD 115200
+
+BME280I2C bme;    // Default : forced mode, standby time = 1000 ms
+                  // Oversampling = pressure ×1, temperature ×1, humidity ×1, filter off,
+
+unsigned long myTime;
+String inputTemp;
+
+//////////////////////////////////////////////////////////////////
+void setup()
+{
+  Serial.begin(SERIAL_BAUD);
+
+  while(!Serial) {} // Wait
+
+  Wire.begin();
+
+  while(!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+    delay(1000);
+  }
+
+  switch(bme.chipModel())
+  {
+     case BME280::ChipModel_BME280:
+       Serial.println("Found BME280 sensor! Success.");
+       break;
+     case BME280::ChipModel_BMP280:
+       Serial.println("Found BMP280 sensor! No Humidity available.");
+       break;
+     default:
+       Serial.println("Found UNKNOWN sensor! Error!");
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    // read the incoming byte:
+    inputTemp = Serial.readString();
+    inputTemp[inputTemp.length()-1] = '\0';
+
+    // get time
+    myTime = millis();
+
+    // say what you got:
+    Serial.print(myTime);
+    Serial.print(", ");
+    Serial.print(inputTemp);
+    Serial.print(", ");
+    printBME280Data(&Serial);
+  }
+}
+
+//////////////////////////////////////////////////////////////////
+void printBME280Data
+(
+   Stream* client
+)
+{
+   float temp(NAN), hum(NAN), pres(NAN);
+
+   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+   bme.read(pres, temp, hum, tempUnit, presUnit);
+
+   client->print(temp);
+   client->print(", ");
+   client->print(hum);
+   client->print(", ");
+   client->print(pres);
+   client->print("\n");
+
+   delay(1000);
+}
